afa744fbe54fbc1425d3611cfba1546c953372da
FranciscoRobles <j-fr_23@hotmail.com>
2018-03-30 00:55:59 -0600

    Full stack

--- src/main.c
+++ src/main.c
@@ -1,34 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 struct StackNode {
-    char c;
+    char data;
     struct StackNode* node;
 };
 
 struct StackNode* newNode(char c){
-    StackNode* root = (StackNode) malloc(sizeof(StackNode));
-    root.c = c;
-    root.node = NULL;
+    struct StackNode* root = (struct StackNode*) malloc(sizeof(struct StackNode));
+    (*root).data = c;
+    (*root).node = NULL;
     return root;
 }
 
-int isEmpty(StackNode* root){
+int isEmpty(struct StackNode *root){
     return !root;
 }
 
-void push(StackNode** root, char c){
-    StackNode* node = newNode(c);
-    node.node = *root;
+void push(struct StackNode** root, char c){
+    struct StackNode* node = newNode(c);
+    (*node).node = *root;
     *root = node;
 }
 
-void pop(StackNode** root){
+int pop(struct StackNode** root){
     if(isEmpty(*root))
     {
-        break;
+        return 0;
     }
-    StackNode* temporal = *root;
-    *root = (*root) node;
+    struct StackNode* temporal = *root;
+    *root = (*root)->node;
     free(temporal);
+    return 1;
+}
+
+char top(struct StackNode* root){
+    if(isEmpty(root))
+    {
+        return 'x';
+    }
+    return (*root).data;
+}
+
+int analizis(struct StackNode** root, char c){
+    if(top(*root) == '{' && c == '}')
+    {
+        pop(root);
+        printf("New top: %c\n", top(*root));
+        return 1;
+    }
+    else if(top(*root) == '[' && c == ']')
+    {
+        pop(root);
+        printf("New top: %c\n", top(*root));
+        return 1;
+    }
+    else if(top(*root) == '(' && c== ')')
+    {
+        pop(root);
+        printf("New top: %c\n", top(*root));
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main(){
+    struct StackNode* root = NULL;
+    printf("Top vacio = %c\n", top(root));
+    push(&root, '(');
+    push(&root, '{');
+    printf("Top element %c\n", top(root));
+    printf("Análizis = %d\n", analizis(&root, '}'));
 }
